5c9e612fc1ee4dcbcc961d133d23d2df85dbf9d6
DazeCake <DazeCake@gmail.com>
2022-05-30 19:56:54 +0800

    Initialization SpringBoot framework

---
+++ src/main/java/moe/dazecake/arklightscloudbackend/ArkLightsCloudBackEndApplication.java
@@ -0,0 +1,13 @@
+package moe.dazecake.arklightscloudbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArkLightsCloudBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArkLightsCloudBackEndApplication.class, args);
+    }
+
+}
