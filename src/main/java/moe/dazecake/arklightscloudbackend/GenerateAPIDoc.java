3edf481bc41412e3166bf86aee0dfa139323fa6c
DazeCake <DazeCake@gmail.com>
2022-05-31 11:16:12 +0800

    add GitHub pages support

--- src/main/java/moe/dazecake/arklightscloudbackend/GenerateAPIDoc.java
+++ src/main/java/moe/dazecake/arklightscloudbackend/GenerateAPIDoc.java
@@ -10,7 +10,7 @@ public class GenerateAPIDoc {
         config.setProjectPath("./"); // 项目根目录
         config.setProjectName("ArkLightsCloudBackEnd"); // 项目名称
         config.setApiVersion("Alpha");       // 声明该API的版本
-        config.setDocsPath("./doc"); // 生成API 文档所在目录
+        config.setDocsPath("./docs"); // 生成API 文档所在目录
         config.setAutoGenerate(Boolean.TRUE);  // 配置自动生成
         config.addPlugin(new MarkdownDocPlugin());
         config.addJavaSrcPath("./src/main/java");
