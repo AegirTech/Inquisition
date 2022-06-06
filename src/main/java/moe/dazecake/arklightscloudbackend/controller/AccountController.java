81ea3e5ac058c3fe004643fcacf6a07c59d606c7
DazeCake <DazeCake@gmail.com>
2022-06-06 21:41:37 +0800

    add DeviceController API and formatting code

--- src/main/java/moe/dazecake/arklightscloudbackend/controller/AccountController.java
+++ src/main/java/moe/dazecake/arklightscloudbackend/controller/AccountController.java
@@ -21,7 +21,7 @@ public class AccountController {
 
     @Operation(summary = "增加账号")
     @PostMapping("/addAccount")
-    public Result<String> addAccount(@RequestBody AccountEntity accountEntity){
+    public Result<String> addAccount(@RequestBody AccountEntity accountEntity) {
         Result<String> result = new Result<>();
 
         accountMapper.insert(accountEntity);
@@ -35,18 +35,18 @@ public class AccountController {
 
     @Operation(summary = "删除账号")
     @PostMapping("/delAccount")
-    public Result<String> delAccount(Long id){
+    public Result<String> delAccount(Long id) {
         Result<String> result = new Result<>();
 
         var account = accountMapper.selectById(id);
-        if (account!= null){
+        if (account != null) {
             account.setDelete(1);
             accountMapper.updateById(account);
 
             result.setCode(200);
             result.setMsg("success");
 
-        }else {
+        } else {
             result.setCode(403);
             result.setMsg("Unable to delete a non-existent account");
 
@@ -58,33 +58,36 @@ public class AccountController {
 
     @Operation(summary = "分页查询账号")
     @GetMapping("/showAccount")
-    public Result<ArrayList<AccountEntity>> showAccount(Long current,Long size){
+    public Result<ArrayList<AccountEntity>> showAccount(Long current, Long size) {
         Result<ArrayList<AccountEntity>> result = new Result<>();
         result.setData(new ArrayList<>());
 
         var data = accountMapper.selectPage(new Page<>(current, size), null);
-        result.getData().addAll(data.getRecords());
+        result.setCode(200)
+                .setMsg("success")
+                .getData()
+                .addAll(data.getRecords());
 
         return result;
     }
 
     @Operation(summary = "更新账号")
     @PostMapping("/updateAccount")
-    public Result<String> updateAccount(Long id,@RequestBody AccountEntity accountEntity) {
+    public Result<String> updateAccount(Long id, @RequestBody AccountEntity accountEntity) {
         Result<String> result = new Result<>();
 
         var account = accountMapper.selectById(id);
-        if (account!= null){
+        if (account != null) {
             account = accountEntity;
             account.setId(id);
             accountMapper.updateById(account);
 
-            result.setCode(200);
-            result.setMsg("success");
+            result.setCode(200)
+                    .setMsg("success");
 
-        }else {
-            result.setCode(403);
-            result.setMsg("Unable to update a non-existent account");
+        } else {
+            result.setCode(403)
+                    .setMsg("Unable to update a non-existent account");
 
         }
         result.setData(null);
---
+++ src/main/java/moe/dazecake/arklightscloudbackend/controller/DeviceController.java
@@ -0,0 +1,102 @@
+package moe.dazecake.arklightscloudbackend.controller;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import moe.dazecake.arklightscloudbackend.entity.AccountEntity;
+import moe.dazecake.arklightscloudbackend.entity.DeviceEntity;
+import moe.dazecake.arklightscloudbackend.mapper.DeviceMapper;
+import moe.dazecake.arklightscloudbackend.util.Result;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+
+@Tag(name = "设备接口")
+@ResponseBody
+@RestController
+public class DeviceController {
+
+    @Resource
+    DeviceMapper deviceMapper;
+
+    @Operation(summary = "增加设备")
+    @PostMapping("/addDevice")
+    public Result<DeviceEntity> addDevice(@RequestBody DeviceEntity deviceEntity) {
+        Result<DeviceEntity> result = new Result<>();
+
+        var deviceToken = RandomStringUtils.randomAlphabetic(16);
+        deviceEntity.setDeviceToken(deviceToken);
+        deviceMapper.insert(deviceEntity);
+
+        result.setCode(200)
+                .setMsg("success")
+                .setData(deviceEntity);
+
+        return result;
+    }
+
+    @Operation(summary = "删除设备")
+    @PostMapping("/delDevice")
+    public Result<String> delDevice(Long id) {
+        Result<String> result = new Result<>();
+        var deviceEntity = deviceMapper.selectById(id);
+
+        if (deviceEntity != null) {
+            deviceEntity.setDelete(1);
+
+            deviceMapper.updateById(deviceEntity);
+
+            result.setCode(200)
+                    .setMsg("success")
+                    .setData(null);
+        } else {
+            result.setCode(403)
+                    .setMsg("A nonexistent device cannot be deleted")
+                    .setData(null);
+        }
+
+        return result;
+    }
+
+    @Operation(summary = "分页查询设备")
+    @GetMapping("/showDevice")
+    public Result<ArrayList<DeviceEntity>> showDevice(Long current, Long size) {
+        Result<ArrayList<DeviceEntity>> result = new Result<>();
+        result.setData(new ArrayList<>());
+
+        var data = deviceMapper.selectPage(new Page<>(current, size), null);
+        result.setCode(200)
+                .setMsg("success")
+                .getData()
+                .addAll(data.getRecords());
+
+        return result;
+    }
+
+    @Operation(summary = "更新设备")
+    @PostMapping("/updateDevice")
+    public Result<String> updateDevice(Long id, @RequestBody DeviceEntity deviceEntity) {
+        Result<String> result = new Result<>();
+
+        var device = deviceMapper.selectById(id);
+        if (device != null) {
+            device = deviceEntity;
+            device.setId(id);
+            deviceMapper.updateById(device);
+
+            result.setCode(200)
+                    .setMsg("success");
+
+        } else {
+            result.setCode(403)
+                    .setMsg("Unable to update a non-existent device");
+
+        }
+        result.setData(null);
+        return result;
+    }
+
+
+}
