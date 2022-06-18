d0c3f931c61965f56cc3d56fa31eb3f89c4ae287
DazeCake <DazeCake@gmail.com>
2022-06-18 14:51:04 +0800

    CORS fix

--- src/main/java/moe/dazecake/inquisition/config/WebConfig.java
+++ src/main/java/moe/dazecake/inquisition/config/WebConfig.java
@@ -2,6 +2,7 @@ package moe.dazecake.inquisition.config;
 
 import moe.dazecake.inquisition.interceptor.JwtTokenInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -22,5 +23,13 @@ public class WebConfig implements WebMvcConfigurer {
                         "/swagger-resources/**",
                         "/v3/api-docs"
                 );
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
     }
 }
