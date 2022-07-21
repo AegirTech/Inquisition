1b8d62c408f9e8fd1e7523e9f36de0cc924222a9
DazeCake <dazecake@gmail.com>
2022-07-21 19:23:40 +0800

    Auto init mysql support

--- src/main/java/moe/dazecake/inquisition/InquisitionApplication.java
+++ src/main/java/moe/dazecake/inquisition/InquisitionApplication.java
@@ -1,12 +1,16 @@
 package moe.dazecake.inquisition;
 
 import moe.dazecake.inquisition.util.DynamicInfo;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@MapperScan({"com.gitee.sunchenbin.mybatis.actable.dao.*", "moe.dazecake.inquisition.mapper"})
+@ComponentScan(basePackages = {"com.gitee.sunchenbin.mybatis.actable.manager.*", "moe.dazecake.inquisition.*"})
 public class InquisitionApplication {
 
     public static void main(String[] args) {
@@ -16,3 +20,5 @@ public class InquisitionApplication {
     }
 
 }
+
+
