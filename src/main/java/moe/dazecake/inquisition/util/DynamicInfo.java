4399c2f039c34a413baddb839e6578a067d28843
DazeCake <dazecake@gmail.com>
2022-07-22 17:45:52 +0800

    Auto init admin account

--- src/main/java/moe/dazecake/inquisition/util/DynamicInfo.java
+++ src/main/java/moe/dazecake/inquisition/util/DynamicInfo.java
@@ -5,8 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import moe.dazecake.inquisition.entity.AccountEntity;
+import moe.dazecake.inquisition.entity.AdminEntity;
 import moe.dazecake.inquisition.entity.DeviceEntity;
 import moe.dazecake.inquisition.mapper.AccountMapper;
+import moe.dazecake.inquisition.mapper.AdminMapper;
 import moe.dazecake.inquisition.mapper.DeviceMapper;
 import org.springframework.stereotype.Component;
 
@@ -14,6 +16,7 @@ import javax.annotation.Resource;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 @Component
 @Data
@@ -26,6 +29,9 @@ public class DynamicInfo {
 
     @Resource
     DeviceMapper deviceMapper;
+
+    @Resource
+    AdminMapper adminMapper;
 
     HashMap<String, Integer> deviceStatusMap = new HashMap<>();
 
@@ -40,6 +46,17 @@ public class DynamicInfo {
     HashMap<String, Integer> counter = new HashMap<>();
 
     public void initInfo() {
+
+        //检查admin表是否有数据
+        List<AdminEntity> adminEntities = adminMapper.selectList(null);
+        if (adminEntities.size() == 0) {
+            AdminEntity adminEntity = new AdminEntity();
+            adminEntity.setUsername("root");
+            adminEntity.setPassword("7966fd2201810e386e8407feaf09b4ea");
+            adminEntity.setPermission("root");
+            adminMapper.insert(adminEntity);
+        }
+
         var devices = deviceMapper.selectList(
                 Wrappers.<DeviceEntity>lambdaQuery()
                         .eq(DeviceEntity::getDelete, 0)
