7a2fc7f5ac3db405191adc934e7ac847d2b3553c
DazeCake <dazecake@gmail.com>
2022-08-22 16:27:03 +0800

    Add SanReport API

---
+++ src/main/java/moe/dazecake/inquisition/controller/SanController.java
@@ -0,0 +1,29 @@
+package moe.dazecake.inquisition.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import moe.dazecake.inquisition.util.DynamicInfo;
+import moe.dazecake.inquisition.util.Result;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@Tag(name = "理智接口")
+@ResponseBody
+@RestController
+public class SanController {
+    @Resource
+    private DynamicInfo dynamicInfo;
+
+    @Operation(summary = "理智上报")
+    @PostMapping("/sanReport")
+    public Result<String> SanReport(Integer san, Integer maxSan, String deviceToken) {
+        Result<String> result = new Result<>();
+        var id = dynamicInfo.getLockTaskList().get(deviceToken).keySet().iterator().next().getId();
+        dynamicInfo.getUserSanList().put(id, san);
+        dynamicInfo.getUserMaxSanList().put(id, maxSan);
+        return result.setCode(200).setMsg("success");
+    }
+}
